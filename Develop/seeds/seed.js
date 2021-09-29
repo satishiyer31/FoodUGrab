68d0674af5ea7ebe838ac684d5a3597af1347e58
satishiyer31 <satishiyer31@gmail.com>
2021-09-28 23:27:56 -0600

    Added connection.js

--- Develop/seeds/seed.js
+++ Develop/seeds/seed.js
@@ -1,4 +1,4 @@
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connections');
 const { Customer, Restaurant, Menu_item } = require('../models');
 
 const CustomerSeedData = require('./CustomerSeedData.json')
