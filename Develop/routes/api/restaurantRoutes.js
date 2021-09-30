3c218defded40766ddff7b9e2e0feb074eee3317
satishiyer31 <satishiyer31@gmail.com>
2021-09-29 19:29:32 -0600

    More on node route

--- Develop/routes/api/restaurantRoutes.js
+++ Develop/routes/api/restaurantRoutes.js
@@ -1,3 +1,33 @@
 const router = require('express').Router();
+// const { json } = require('sequelize/types');
+const { Menu_item,Restaurant } = require('../../models');
+// const {Restaurant} = require('../../models/restaurant');
+
+//Get all restaurants
+router.get('/',async(req,res) => {
+    try{
+        const restaurants = await Restaurant.findAll();
+        res.status(200).json(restaurants);
+    }
+    catch(err){
+        json.status(500).json(err);
+    }
+
+});
+
+//Get restuarant by ID
+router.get('/:id',async(req,res) => {
+    try{
+        const restaurants = await Restaurant.findByPk(req.params.id, {include: [{model:Menu_item}]});
+        res.status(200).json(restaurants);
+    }
+    catch(err){
+        json.status(500).json(err);
+    }
+
+});
+
+
+
 
 module.exports = router;
