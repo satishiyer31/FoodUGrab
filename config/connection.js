70114d4ad709b89550d987f0e202d35184bd1948
satishiyer31 <satishiyer31@gmail.com>
2021-10-20 22:04:48 -0600

    Added if statement for connectoin.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,12 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-
-  const sequelize = new Sequelize(
+if (process.env.JAWSDB_URL){
+  const sequelize = new Sequelize(process.env.JAWSDB_URL)
+    
+}
+ else{
+    const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
@@ -12,6 +16,7 @@ require("dotenv").config();
       port: 3306,
     }
   );
+ } 
 
 
 module.exports = sequelize;
